2d4fb269723d1ea42445424abd8d0a43d99531e4
bcwaters <bcwaters@asu.edu>
2025-03-05 19:34:40 -0800

    updated markdown css

---
+++ grok/utils/LatexRenderer.js
@@ -0,0 +1,17 @@
+
+
+// detectPossibleLatex(response){
+//     //detect possible latex in the response
+//     //return true if latex is detected
+//     //return false otherwise
+//     //a string that starts with $ and ends with $ is a latex expression
+//     //a string that starts with $$ and ends with $$ is a latex expression
+//     //a string that starts with \ and ends with \ is a latex expression
+//     //a string that starts with \begin{ and ends with \end{ is a latex expression
+//     //a string that starts with \begin{equation} and ends with \end{equation} is a latex expression
+//     //a string that starts with \begin{align} and ends with \end{align} is a latex expression
+//     //a string that starts with \begin{align*} and ends with \end{align*} is a latex expression
+
+
+//     return response.includes('\\');
+// }
